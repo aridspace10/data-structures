1f21a96124b26d366dbe0a407d8a4fbdcda2fea1
aridspace10 <jackovand27@icloud.com>
2024-09-21 12:47:36 +1000

    Added resize and append functions

--- C++/dynamic-array.cpp
+++ C++/dynamic-array.cpp
@@ -6,11 +6,27 @@ class DynamicArray {
     private:
         int start;
         int end;
-        int size;
+        int length;
         int capacity;
 
         int getCapacity() {
             return capacity;
+        }
+
+        void __resize(int new_capacity) {
+            capacity = new_capacity;
+            std::vector<T>* new_list = [NULL] * (capacity);
+            //Reset pointer to put content in middle of array with equal memory
+            // buffers on both size
+            int new_start = self.capacity;
+            int new_end = new_start + length - 1;
+            // Copy both lists to new bigger array
+            for (int i = 0; i < length; i++) {
+                new_list[new_start + i] = data[start + i];
+            }
+            data = new_list;
+            start = new_start;
+            end = new_end;
         }
 
     public:
@@ -20,11 +36,21 @@ class DynamicArray {
             start = 2;
             end = 1;
             capacity = 4;
-            size = 0;
+            length = 0;
         }
+
         int getLength() {
-            return size;
+            return length;
         }
+
+        void append(<T> value) {
+            if (data[getCapacity() - 1] != NULL) {
+                __resize(getCapacity() * 2);
+            }
+            end += 1;        
+            length += 1;
+        }
+
 };
 
 int main() {
