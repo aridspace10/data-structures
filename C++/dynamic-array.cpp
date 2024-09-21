5537e9c58d117a7fc3975db4efbc8e930328bbeb
aridspace10 <jackovand27@icloud.com>
2024-09-21 16:31:27 +1000

    Added get, set_at, display

--- C++/dynamic-array.cpp
+++ C++/dynamic-array.cpp
@@ -8,6 +8,7 @@ class DynamicArray {
         int end;
         int length;
         int capacity;
+        std::vector<T> data;
 
         int getCapacity() {
             return capacity;
@@ -15,10 +16,10 @@ class DynamicArray {
 
         void __resize(int new_capacity) {
             capacity = new_capacity;
-            std::vector<T>* new_list = [NULL] * (capacity);
+            std::vector<T> new_list(new_capacity);
             //Reset pointer to put content in middle of array with equal memory
             // buffers on both size
-            int new_start = self.capacity;
+            int new_start = capacity;
             int new_end = new_start + length - 1;
             // Copy both lists to new bigger array
             for (int i = 0; i < length; i++) {
@@ -30,29 +31,73 @@ class DynamicArray {
         }
 
     public:
-        std::vector<T>* data;
-
         DynamicArray() {
             start = 2;
             end = 1;
             capacity = 4;
             length = 0;
+            data = {NULL, NULL, NULL, NULL};
+        }
+
+        void display() {
+            for (int i = start; i < end + 1; i++) {
+                std::cout << data[i];
+            }
         }
 
         int getLength() {
             return length;
         }
 
-        void append(<T> value) {
-            if (data[getCapacity() - 1] != NULL) {
+        void append(const T& value) {
+            if (end == getCapacity() - 1) {
                 __resize(getCapacity() * 2);
             }
-            end += 1;        
+            end += 1;
+            set_at(-1, value);
             length += 1;
         }
 
+        //allows DynamicArray[] operation
+        int& operator[](int index) {
+            if (index < 0 || index >= getLength()) {
+                throw std::out_of_range("Index out of bounds");
+            }
+            return data[index];
+        }
+
+        const T& get_at(int index) {
+            if (index >= 0) {
+                if (index > getLength()) {
+                    throw std::out_of_range("Index out of bounds");
+                }
+                return data[start + index];
+            } else {
+                if (-index > getLength) {
+                    throw std::out_of_range("Index out of bounds");
+                }
+                return data[end + index + 1];
+            }
+        }
+
+        void set_at(int index, const T& value) {
+            if (index >= 0) {
+                if (index > getLength()) {
+                    throw std::out_of_range("Index out of bounds");
+                }
+                data[start + index] = value;
+            } else {
+                if (-index - 1 > getLength()) {
+                    throw std::out_of_range("Index out of bounds");
+                }
+                data[end + index + 1] = value;
+            }
+        }
 };
 
 int main() {
+    DynamicArray<int> arr;
+    arr.append(5);
+    arr.display();
     return 0;
 }
