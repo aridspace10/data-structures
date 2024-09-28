d873ea60da5ae0160ed7feeb0293fad96dfac7a7
aridspace10 <jackovand27@icloud.com>
2024-09-29 00:04:02 +1000

    made basic hash table setup, need linked list to complete

---
+++ C++/hash-table.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "dynamic-array.cpp"
+template <typename T>
+class HashTable {
+    private:
+        int size;
+        DynamicArray<T> arr;
+    public:
+        int getSize() {
+            return size;
+        }
+
+        int insert() {
+            ;
+        }
+};
+
+int main() {
+    return 0;
+}
